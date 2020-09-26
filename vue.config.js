a3c98f16b9220e05237f64ef032e5a3466682b0a
hyl <hyl4@meitu.com>
2020-09-26 19:41:37 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,21 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = {
-    publicPath: './',
-    // devServer: {
-    //     open: true,
-    //     proxy: {  //配置跨域
-    //         '/quant': {
-    //             target: 'http://gatro.net:4041',  //这里后台的地址模拟的;应该填写你们真实的后台接口
-    //             changOrigin: true,  //允许跨域
-    //             onProxyRes: function (proxyRes, req, res) { // 代理response事件
-    //                 console.log('res---->\n\n\n')
-    //                 // console.log(proxyRes.headers)
-    //             },
-    //             onProxyReq: function (proxyReq, req, res) { // 代理requset事件
-    //                 console.log('req---->\n\n\n')
-    //                 delete req.headers.host
-    //             },
-    //         },
-    //     }
-    // },
+    devServer: {
+        open: true,
+        proxy: {  //配置跨域
+            '/quant': {
+                target: 'http://api.gatro.net',  //这里后台的地址模拟的;应该填写你们真实的后台接口
+                changOrigin: true,  //允许跨域
+                onProxyRes: function (proxyRes, req, res) { // 代理response事件
+                    console.log('res---->\n\n\n')
+                    // console.log(proxyRes.headers)
+                },
+                onProxyReq: function (proxyReq, req, res) { // 代理requset事件
+                    console.log('req---->\n\n\n')
+                    delete req.headers.host
+                },
+            },
+        }
+    },
 }
