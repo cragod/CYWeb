5f36e82a6fef1048b294c08d3c67ed6337a247f1
hyl <hyl4@meitu.com>
2020-09-27 10:40:22 +0800

    rebuild

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,37 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
     publicPath: './',
     css: { extract: false },
     chainWebpack: config => {
         // 删除这个 preload 插件。
         config.plugins.delete('preload');
+    },
+    // 配置webpack
+    configureWebpack: config => {
+        if (isProduction) {
+            // 开启分离js
+            config.optimization = {
+                runtimeChunk: 'single',
+                splitChunks: {
+                    chunks: 'all',
+                    maxInitialRequests: Infinity,
+                    minSize: 20000,
+                    cacheGroups: {
+                        vendor: {
+                            test: /[\\/]node_modules[\\/]/,
+                            name(module) {
+                                // get the name. E.g. node_modules/packageName/not/this/part.js
+                                // or node_modules/packageName
+                                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                                // npm package names are URL-safe, but some servers don't like @ symbols
+                                return `npm.${packageName.replace('@', '')}`
+                            }
+                        }
+                    }
+                }
+            };
+        }
     },
     devServer: {
         open: true,
