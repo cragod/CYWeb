3773c5aa17003abd3a21c9399e600c1b558a6f9c
hyl <hyl4@meitu.com>
2020-09-27 09:50:36 +0800

    delete unused config

--- vue.config.js
+++ vue.config.js
@@ -11,14 +11,6 @@ module.exports = {
             '/quant': {
                 target: 'http://api.gatro.net',  //这里后台的地址模拟的;应该填写你们真实的后台接口
                 changOrigin: true,  //允许跨域
-                onProxyRes: function (proxyRes, req, res) { // 代理response事件
-                    console.log('res---->\n\n\n')
-                    // console.log(proxyRes.headers)
-                },
-                onProxyReq: function (proxyReq, req, res) { // 代理requset事件
-                    console.log('req---->\n\n\n')
-                    delete req.headers.host
-                },
             },
         }
     },
