2febc8966f4bb6e6a4ed56d571d6ca408b5f9870
黄云龙 <hyl4@meitu.com>
2020-12-10 00:23:38 +0800

    update style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <a-layout>
+  <a-layout id="layout">
     <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
-      <!-- <div class="logo" /> -->
+      <div class="logo">
+        <span class="title">CYQuant</span>
+      </div>
       <a-menu
         theme="dark"
         mode="horizontal"
@@ -43,9 +45,24 @@ export default {
 </script>
 
 <style scoped>
+#layout .logo {
+  width: 120px;
+  height: 31px;
+  margin: 16px 28px 16px -15px;
+  float: left;
+}
 .content {
   padding: 0px 5px;
   min-height: "380px";
   background: white;
+}
+.title {
+  line-height: 1;
+  font-size: 200%;
+  font-weight: 900;
+  font-style: oblique;
+  text-decoration: underline white;
+  color: ghostwhite;
+  user-select: none;
 }
 </style>
